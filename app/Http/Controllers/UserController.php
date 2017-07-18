890749c909068e17b3598cf4ffee33d2c3fc5664
jamesnogra <james.nogra@ripeconcepts.com>
2017-07-18 12:15:41 +0800

    Custom register and login done.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Hash;
+use App\User;
+
+class UserController extends Controller
+{
+
+    public function all()
+    {
+        $all_users = User::all();
+        return view('auth/all', compact('all_users'));
+    }
+
+    public function login()
+    {
+        return view('auth/login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return redirect(action('UserController@login'));
+        }
+        return Auth::user();
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect(action('UserController@login'));
+    }
+
+    public function register()
+    {
+        return view('auth/register');
+        return redirect(action('UserController@login'));
+    }
+
+    public function postRegister(Request $request)
+    {
+        $new_user = new User();
+        $new_user->name = $request->name;
+        $new_user->email = $request->email;
+        $new_user->password = Hash::make($request->password);
+        $new_user->type = $request->type;
+        $new_user->save();
+        return $new_user;
+    }
+
+}
---
+++ resources/views/auth/all.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">All Users</div>
+                <div class="panel-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Type</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                        <tbody>
+                            @foreach ($all_users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->type }}</td>
+                                    <td>
+                                        <button class="btn btn-warning btn-xs">Edit</button>
+                                        <button class="btn btn-danger btn-xs">Delete</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
